81d6f82fe534615e6f776ba4168a235bebe80d89
ohtaeg <otk1090@naver.com>
2020-01-11 23:23:30 +0900

    feat (add generic) : openApi 타입에 대한 객체만 사용할 수 있도록 제네릭 타입 변경

--- naver-api-executor/src/main/java/me/ohtaeg/domain/search/repository/SearchRepository.java
+++ naver-api-executor/src/main/java/me/ohtaeg/domain/search/repository/SearchRepository.java
@@ -1,18 +1,19 @@
 package me.ohtaeg.domain.search.repository;
 
 import me.ohtaeg.api.dto.SearchWord;
-import me.ohtaeg.domain.response.Blog;
+import me.ohtaeg.domain.response.OpenApi;
 import me.ohtaeg.domain.search.RequestVariableName;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
-public interface SearchRepository {
+public interface SearchRepository<T extends OpenApi> {
     String UTF_8 = "UTF-8";
 
-    Blog search(SearchWord searchWord) throws UnsupportedEncodingException;
+    ResponseEntity<T> search(SearchWord searchWord) throws UnsupportedEncodingException;
 
     // TODO : refactor - repo interface가 header에 대한 default 메서드를 갖고있는게 맞을까?
     default HttpHeaders getHeader(String id, String secret) {
@@ -22,11 +23,12 @@ public interface SearchRepository {
         return httpHeaders;
     }
 
-    default UriComponentsBuilder getUriBuilder(final String url, final SearchWord searchWord) throws UnsupportedEncodingException {
+    default UriComponents getUriBuilder(final String url, final SearchWord searchWord) {
         return UriComponentsBuilder.fromHttpUrl(url)
-                .queryParam(RequestVariableName.QUERY.getName(), URLEncoder.encode(searchWord.getQuery(), UTF_8))
+                .queryParam(RequestVariableName.QUERY.getName(), searchWord.getQuery())
                 .queryParam(RequestVariableName.DISPLAY.getName(), searchWord.getDisplay())
                 .queryParam(RequestVariableName.START.getName(), searchWord.getStart())
-                .queryParam(RequestVariableName.SORT.getName(), searchWord.getSort());
+                .queryParam(RequestVariableName.SORT.getName(), searchWord.getSort())
+                .build();
     }
 }
