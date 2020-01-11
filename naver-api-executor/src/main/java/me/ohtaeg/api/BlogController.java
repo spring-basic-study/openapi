f28f6ba7acc4db949fd334e928109aea8b12de84
ohtaeg <otk1090@naver.com>
2020-01-11 23:25:01 +0900

    feat (add ResponseEntity) : 결과값을 ResponseEntity로 변경

--- naver-api-executor/src/main/java/me/ohtaeg/api/BlogController.java
+++ naver-api-executor/src/main/java/me/ohtaeg/api/BlogController.java
@@ -5,6 +5,7 @@ import me.ohtaeg.api.dto.SearchWord;
 import me.ohtaeg.domain.response.Blog;
 import me.ohtaeg.domain.search.repository.SearchRepository;
 import me.ohtaeg.infra.BlogRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,14 +15,14 @@ import java.io.UnsupportedEncodingException;
 @RestController
 @RequestMapping("/api")
 public class BlogController {
-    private SearchRepository repository;
+    private final SearchRepository repository;
 
     public BlogController(BlogRepository blogRepository) {
         this.repository = blogRepository;
     }
 
     @GetMapping("/blog")
-    public Blog blog(@BlogArgumentResolver SearchWord searchWord) throws UnsupportedEncodingException {
+    public ResponseEntity<Blog> blog(@BlogArgumentResolver SearchWord searchWord) throws UnsupportedEncodingException {
         return repository.search(searchWord);
     }
 }
