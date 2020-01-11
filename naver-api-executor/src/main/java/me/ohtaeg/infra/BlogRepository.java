f0fb77b6ff71dc11dcefceaa4538417542a89bb2
ohtaeg <otk1090@naver.com>
2020-01-11 23:22:58 +0900

    feat (change uriComponet) : UriComponetsBuilder를 UriComponents로 변경 - build() 하지 않으면 알아서 인코딩되는 내부 소스 확인으로 인해 인코딩 필요 없이 값을 보내기 위함.

--- naver-api-executor/src/main/java/me/ohtaeg/infra/BlogRepository.java
+++ naver-api-executor/src/main/java/me/ohtaeg/infra/BlogRepository.java
@@ -1,20 +1,19 @@
 package me.ohtaeg.infra;
 
 import config.NaverApiProperties;
-import me.ohtaeg.domain.response.Blog;
 import me.ohtaeg.api.dto.SearchWord;
+import me.ohtaeg.domain.response.Blog;
 import me.ohtaeg.domain.search.repository.SearchRepository;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.io.UnsupportedEncodingException;
+import org.springframework.web.util.UriComponents;
 
 @Repository
-public class BlogRepository implements SearchRepository {
+public class BlogRepository implements SearchRepository<Blog> {
 
     // TODO : refactoring - Repository가 properties와 restTemplate를 갖고있는게 맞을까?
     private NaverApiProperties naverApiProperties;
@@ -26,14 +25,13 @@ public class BlogRepository implements SearchRepository {
     }
 
     @Override
-    public Blog search(final SearchWord searchWord) throws UnsupportedEncodingException {
-        UriComponentsBuilder builder = getUriBuilder(naverApiProperties.getUrl(), searchWord);
-
+    public ResponseEntity<Blog> search(final SearchWord searchWord) {
+        UriComponents uriComponents = getUriBuilder(naverApiProperties.getUrl(), searchWord);
         HttpHeaders httpHeaders = getHeader(naverApiProperties.getClientId(), naverApiProperties.getClientSecret());
 
-        return restTemplate.exchange(builder.toUriString()
-                                     , HttpMethod.GET
-                                     , new HttpEntity<>(httpHeaders)
-                                     , Blog.class).getBody();
+        return restTemplate.exchange(uriComponents.toUriString()
+                                      , HttpMethod.GET
+                                      , new HttpEntity<>(httpHeaders)
+                                      , Blog.class);
     }
 }
