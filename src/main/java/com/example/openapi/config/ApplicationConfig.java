0a515f5155de8b5375d8c36c5ef505321a6c460d
황선아 <tjsdk2769@gmail.com>
2020-01-15 09:57:47 +0900

    refactor : ApplicationConfig.java 삭제

--- src/main/java/com/example/openapi/config/ApplicationConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.openapi.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.example.openapi")
-public class ApplicationConfig {
-}
