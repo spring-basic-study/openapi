a533fb66a536e1ffb0cc788f760a3b37d209e09a
ohtaeg <otk1090@naver.com>
2020-01-16 19:34:10 +0900

    refactor (controller) : 블로그, 영화, 블로그 + 영화 검색 추가

--- naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
+++ naver-api-executor/src/main/java/me/ohtaeg/api/ApiController.java
@@ -1,13 +1,65 @@
 package me.ohtaeg.api;
 
+import me.ohtaeg.api.dto.BlogRequest;
+import me.ohtaeg.api.dto.MovieRequest;
+import me.ohtaeg.api.dto.SearchWord;
+import me.ohtaeg.application.BlogService;
+import me.ohtaeg.application.MovieService;
+import me.ohtaeg.domain.response.Blog;
+import me.ohtaeg.domain.response.Movie;
+import me.ohtaeg.domain.response.SearchApi;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
+@RequestMapping("/api")
 public class ApiController {
 
-    @GetMapping("/")
-    public String defaultUrl() {
-        return "hello world, please rewrite /blog + /{query}";
+    private BlogService blogService;
+    private MovieService movieService;
+
+    public ApiController(final BlogService blogService, final MovieService movieService) {
+        this.blogService = blogService;
+        this.movieService = movieService;
+    }
+
+    @GetMapping("/blog")
+    public Blog blog(@Valid BlogRequest blogRequest) {
+        return blogService.search(blogRequest);
+    }
+
+    @GetMapping("/movie")
+    public Movie movie(@Valid MovieRequest movieRequest) {
+        return movieService.search(movieRequest);
+    }
+
+    @GetMapping("/with")
+    public List<SearchApi> with(@Valid SearchWord searchWord) {
+        // TODO : refactoring
+        List<SearchApi> searchApis = new ArrayList<>();
+        String query = searchWord.getQuery();
+        int start = searchWord.getStart();
+        int display = searchWord.getDisplay();
+
+        BlogRequest blogRequest = new BlogRequest(query);
+        blogRequest.setStart(start);
+        blogRequest.setDisplay(display);
+
+        MovieRequest movieRequest = new MovieRequest(query);
+        movieRequest.setStart(start);
+        movieRequest.setDisplay(display);
+
+        Blog blog = blogService.search(blogRequest);
+        Movie movie = movieService.search(movieRequest);
+
+        searchApis.add(blog);
+        searchApis.add(movie);
+
+        return searchApis;
     }
 }
