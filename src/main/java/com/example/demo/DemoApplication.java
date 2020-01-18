e4b7bcd6b75d890e701f728fb1e04c89ee83012f
Eddy.Kim <sieunkr@gmail.com>
2020-01-18 14:32:29 +0900

    step02 개발완료 커밋

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,7 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
         //수정사항을 반영해서 다시 PUSH
+        //STEP02 에 대한 수정사항 개발 완료
     }
 
 }
