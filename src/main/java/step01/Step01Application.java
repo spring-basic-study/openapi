0417b4ad26f92d274e84b4b730c3b0f2f76dce7a
Hong-Jun Eom <nara1030@naver.com>
2020-01-22 18:31:21 +0900

    STEP01 코드 초안

---
+++ src/main/java/step01/Step01Application.java
@@ -0,0 +1,11 @@
+package step01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Step01Application.class, args);
+    }
+}
---
+++ src/main/java/step01/repository/response/BlogResponse.java
@@ -0,0 +1,10 @@
+package step01.repository.response;
+
+import lombok.Getter;
+
+@Getter
+public class BlogResponse {
+    private String title;
+    private String link;
+    private String bloggerName;
+}
