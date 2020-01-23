6053f609d35b518fa8ac8c41763bbcbd24cb56a7
ohtaeg <otk1090@naver.com>
2020-01-23 15:54:15 +0900

    refactor (remove setter) : setter 제거 및 기본생성자 접근제어자 변경

--- naver-api-executor/src/main/java/me/ohtaeg/domain/response/Blog.java
+++ naver-api-executor/src/main/java/me/ohtaeg/domain/response/Blog.java
@@ -14,15 +14,19 @@ public class Blog extends SearchApi implements Limitable {
     private static final String SEARCH_RESOURCE = "/blog";
     private List<Item> items;
 
-    public Blog() { }
+    protected Blog() { }
     public Blog(final SearchWord searchWord) {
         super.uri= createUri(searchWord);
+    }
+
+    public List<Item> getItems() {
+        return items;
     }
 
     @Override
     String createUri(final SearchWord searchWord) {
         BlogRequest blogRequest = (BlogRequest) searchWord;
-        return UriComponentsBuilder.fromHttpUrl(PropertyUtils.getUrl() + SEARCH_RESOURCE)
+        return UriComponentsBuilder.fromHttpUrl(PropertyUtils.getInstance().getUrl() + SEARCH_RESOURCE)
                 .queryParam(RequestVariableName.QUERY.getName(), blogRequest.getQuery())
                 .queryParam(RequestVariableName.DISPLAY.getName(), blogRequest.getDisplay())
                 .queryParam(RequestVariableName.START.getName(), blogRequest.getStart())
@@ -39,8 +43,8 @@ public class Blog extends SearchApi implements Limitable {
 
         super.limitLength = length;
         this.items = items.stream()
-                          .limit(length)
-                          .collect(Collectors.toList());
+                .limit(length)
+                .collect(Collectors.toList());
     }
 
     private static class Item {
@@ -54,47 +58,20 @@ public class Blog extends SearchApi implements Limitable {
             return title;
         }
 
-        public void setTitle(final String title) {
-            this.title = title;
-        }
-
         public String getLink() {
             return link;
-        }
-
-        public void setLink(final String link) {
-            this.link = link;
         }
 
         public String getDescription() {
             return description;
         }
 
-        public void setDescription(final String description) {
-            this.description = description;
-        }
-
         public String getBloggername() {
             return bloggername;
-        }
-
-        public void setBloggername(final String bloggername) {
-            this.bloggername = bloggername;
         }
 
         public String getBloggerlink() {
             return bloggerlink;
         }
-
-        public void setBloggerlink(final String bloggerlink) {
-            this.bloggerlink = bloggerlink;
-        }
-    }
-
-    public List<Item> getItems() {
-        return items;
-    }
-    public void setItems(final List<Item> items) {
-        this.items = items;
     }
 }
--- naver-api-executor/src/main/java/me/ohtaeg/domain/response/Movie.java
+++ naver-api-executor/src/main/java/me/ohtaeg/domain/response/Movie.java
@@ -15,16 +15,19 @@ public class Movie extends SearchApi implements Limitable {
     private static final String SEARCH_RESOURCE = "/movie";
     private List<Item> items;
 
-    public Movie() { }
-
+    protected Movie() { }
     public Movie(final SearchWord searchWord) {
         super.uri = createUri(searchWord);
+    }
+
+    public List<Item> getItems() {
+        return items;
     }
 
     @Override
     String createUri(final SearchWord searchWord) {
         MovieRequest movieRequest = (MovieRequest) searchWord;
-        return UriComponentsBuilder.fromHttpUrl(PropertyUtils.getUrl() + SEARCH_RESOURCE)
+        return UriComponentsBuilder.fromHttpUrl(PropertyUtils.getInstance().getUrl() + SEARCH_RESOURCE)
                 .queryParam(RequestVariableName.QUERY.getName(), movieRequest.getQuery())
                 .queryParam(RequestVariableName.DISPLAY.getName(), movieRequest.getDisplay())
                 .queryParam(RequestVariableName.START.getName(), movieRequest.getStart())
@@ -54,47 +57,20 @@ public class Movie extends SearchApi implements Limitable {
             return title;
         }
 
-        public void setTitle(final String title) {
-            this.title = title;
-        }
-
         public String getLink() {
             return link;
-        }
-
-        public void setLink(final String link) {
-            this.link = link;
         }
 
         public String getSubtitle() {
             return subtitle;
         }
 
-        public void setSubtitle(final String subtitle) {
-            this.subtitle = subtitle;
-        }
-
         public String getActor() {
             return actor;
-        }
-
-        public void setActor(final String actor) {
-            this.actor = actor;
         }
 
         public String getUserRating() {
             return userRating;
         }
-
-        public void setUserRating(final String userRating) {
-            this.userRating = userRating;
-        }
-    }
-
-    public List<Item> getItems() {
-        return items;
-    }
-    public void setItems(final List<Item> items) {
-        this.items = items;
     }
 }
